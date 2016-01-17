7a8d72c3070e39d52e7e2d2e52706ccbe3197d7f
David256 <davidborja456@gmail.com>
2016-01-17 14:35:32 -0500

    Javadoc dento de la clase Box

--- src/interfaz/Box.java
+++ src/interfaz/Box.java
@@ -3,7 +3,11 @@ package interfaz;
 import java.awt.*;
 import java.util.*;
 
-
+/**
+* Esta clase define los métodos y atributos para el manejo y dibujo de
+* los cubos en el juego.
+*
+*/
 public class Box
 {
 	private final Color color;
@@ -12,6 +16,17 @@ public class Box
 	private int limite;
 	private boolean encima = true;
 
+
+
+	/**
+	* Contructor de la clase Box.
+	*
+	* @param posicion 	indica la posición del cubo en la distribución de cubos.
+	*					Esta no es la posición en pixeles, sino la de los cubos.
+	*
+	* @param altitud 	indica la altitud del cubo en la distribución de cubos.
+	*					Esta no es la altitud en pixeles, sino la de los cubos.
+	*/
 	public Box(int posicion, int altitud)
 	{
 		this.posicion = posicion;
@@ -30,21 +45,81 @@ public class Box
 		this.color = elColor[i];
 	}
 
+
+
+	/**
+	* Fija el valor de la posición nueva ue tendrá el cubo.
+	*
+	* @param neoPos 	Es el valor de la nueva posición.
+	*/
 	public void setPosicion(int neoPos)
 	{
 		this.posicion = neoPos;
 	}
+
+
+
+	/**
+	* Fija el valor de la altitud nueva ue tendrá el cubo.
+	*
+	* @param neoAlt	Es el valor de la nueva altitud.
+	*/
 	public void setAltitud(int neoAlt)
 	{
 		this.altitud = neoAlt;
 	}
+
+
+	/**
+	* Fija el valor limite para desplazar los cubos.
+	*
+	* @param limite
+	*/
+	public void setLimite(int limite)
+	{
+		this.limite = limite;
+	}
+
+
+
+	/**
+	* Retorna el valor de la posicion de el objecto de la clase Box
+	*
+	* @return Retorna un entero con el valor de la posición de derecha a izquierda
+	*/
 	public int getPosicion(){ return this.posicion; }
+
+
+
+	/**
+	* Retorna el valor de la altitud de el objecto de la clase Box
+	*
+	* @return Retorna un entero con el valor de la altitud de abajo a arriba
+	*/
 	public int getAltitud(){ return this.altitud; }
 
+
+
+	/**
+	* Este método retorno un valor unico para cada objecto de la clase Box
+	*
+	* @return String que contiene un valor unico por cubo, 
+	*		  al menos que dos de estos tengan la misma posición en el espacio
+	*/
 	public String getNick(){
 		return "x" + this.posicion + "y" + this.altitud;
 	}
 
+
+
+	/**
+	* Dibuja en un objecto de la clase Graphics el cubo, todo en su respectiva posición
+	* en el espacio.
+	*
+	* @param g 		Es un objecto de la clase Graphics para poder dibujar en pantalla
+	*
+	* @see java.awt.Graphics
+	*/
 	public void dibujar(Graphics g)
 	{
 		if (this.encima)
@@ -68,6 +143,17 @@ public class Box
 			g.drawRect(x, y, 20, 20);
 		}
 	}
+
+
+
+	/**
+	* Con este método podremos saber si un color es igual al que tiene
+	* un objecto de la clase Box.
+	*
+	* @return boolean que define si es verdadero, siendo los colores iguales.
+	*		  o falso si no lo son.
+	*
+	*/
 	public boolean esTuColor(Color koloro)
 	{
 		if (this.color.equals(koloro))
@@ -76,16 +162,25 @@ public class Box
 			return false;
 	}
 
+
+	/**
+	* Nos permite avanzar una posición hacia la izquierda, haciendo que los cubos
+	* se desplacen
+	*/
 	public void avanzar()
 	{
 		this.posicion++;
 	}
 
-	public void setLimite(int limite)
-	{
-		this.limite = limite;
-	}
 
+	/**
+	* Este método gestiona la activación de la animación de caida solo si
+	* el valor de la posición del cubo ha sobrepasado el limite permitido.
+	* El no uso de este método evitará que los cubos caigan, lo que significa
+	* que no tendrá gravedad.
+	*
+	* @see #setLimite(int limite)
+	*/
 	public void comprobarCaida()
 	{
 		if (this.limite == (680 - (this.posicion*20)))
